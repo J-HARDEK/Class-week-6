215d6c2670d0e4d24ccc9667ea057b23d5c1a0ff
John Hardek <john.hardek@kellogg.northwestern.edu>
2025-02-13 02:51:22 +0000

    finished class

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/tacos", :controller => "tacos", :action => "index")
   get("/dice", :controller => "dice", :action => "index2")
+  get("/companies", :controller => "companies", :action => "index")
 end
